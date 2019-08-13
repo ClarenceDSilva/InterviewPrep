9f643c2438b474f7538f4e6f6d08f88bf794712c
Clarence D'Silva <clarence.dsilva@hotmail.com>
2019-08-13 16:38:10 -0400

    Java8Lambda files

---
+++ src/ds/Java8Lambda/Greeting.java
@@ -0,0 +1,3 @@
+public interface Greeting {
+	public void performInterface();
+}
---
+++ src/ds/Java8Lambda/HelloWorldGreeter.java
@@ -0,0 +1,11 @@
+
+public class HelloWorldGreeter implements Greeting{
+
+	@Override
+	public void performInterface() {
+		System.out.println("Hello World from the Greeting Interface");
+		
+	}
+
+
+}
